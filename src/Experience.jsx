e98c5d4ce35d5567d8f6dc93106459804e455fde
alessandrobattiato <alessandroDev@outlook.it>
2024-08-24 12:34:17 +0200

    feat: replace orbit controls with presentation controls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,4 +1,9 @@
-import { Float, Environment, OrbitControls, useGLTF } from "@react-three/drei";
+import {
+    PresentationControls,
+    Float,
+    Environment,
+    useGLTF,
+} from "@react-three/drei";
 
 export default function Experience() {
     // CC0 licensed free model by pmndrs market
@@ -13,11 +18,19 @@ export default function Experience() {
 
             <color attach="background" args={["#241a1a"]} />
 
-            <OrbitControls makeDefault />
-
-            <Float rotationIntensity={0.4}>
-                <primitive position-y={-1.2} object={computer.scene} />
-            </Float>
+            {/*
+                The presentation controls is a component that internally uses the library Spring for pleasant animations,
+                and we currently have removed the OrbitControls as to reduce the freedom of the user to drag and drop the model
+                in fact, now the user can still do this but then the model returns to it's original position
+                
+                The global attribute makes the user able to drag and drop the model from anywhere, even from outside the model "scope"
+                it makes sense to be able to do this since the model will be the main component of our page
+            */}
+            <PresentationControls global>
+                <Float rotationIntensity={0.4}>
+                    <primitive position-y={-1.2} object={computer.scene} />
+                </Float>
+            </PresentationControls>
         </>
     );
 }
