51ff1da777040552a73dd9c8b87d30e09f9158f6
alessandrobattiato <alessandroDev@outlook.it>
2024-08-24 12:39:47 +0200

    fix: fix mobile issue regarding touch actions

--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,11 @@ import Experience from "./Experience.jsx";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
+// In order to fix the mobile gestures issues when dragging the model, you need to apply the touch-action none to the div that is implicitly injected by R3F as a wrapper of the Canvas element, and you can do so by giving a className to the Canvas component that's why you gave an R3F className
+
 root.render(
     <Canvas
+        className="r3f"
         camera={{
             fov: 45,
             near: 0.1,
